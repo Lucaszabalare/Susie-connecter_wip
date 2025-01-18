8a951a34e5f3294f30fd211794bc6f63fac3899b
Lucaszabalare <nellymarac@gmail.com>
2025-01-18 11:17:21 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -8,9 +8,8 @@ TITLE = "Susie Connect"
 
 start_time = 0
 total_time = 0
-end_time = 0
 number_of_cats = 10
-next_cat = 0
+current_cat = 0
 
 cats = []
 lines = []
@@ -29,12 +28,34 @@ def draw():
 
     num = 1
     for susie in cats:
-        screen.draw.text(str(num),(susie.pos[0],susie.pos[1] + 20))
+        screen.draw.text(str(num),(susie.pos[0],susie.pos[1] + 30))
         susie.draw()
         num = num + 1
     
+    for line in lines:
+        screen.draw.line(line[0],line[1],(255,255,255))
+    
+    if current_cat < number_of_cats:
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time,1)),(15,15),fontsize = 30)
+    else:
+        screen.draw.text(str(round(total_time,1)),(15,15),fontsize = 30)
+    
+    
 
+def update():
+    pass
 
+def on_mouse_down(pos):
+    global current_cat, lines
+    if current_cat < number_of_cats:
+        if cats[current_cat].collidepoint(pos):
+            if current_cat:
+                lines.append((cats[current_cat - 1].pos , cats[current_cat].pos))
+            current_cat = current_cat + 1
+        else:
+            lines = []
+            current_cat = 0
 
 creat_cats()
 pgzrun.go()
